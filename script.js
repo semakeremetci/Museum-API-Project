dc92f6d75194a573a45dafd420ad10c54067cb77
semakeremetci <keremetcisema@gmail.com>
2022-11-15 20:15:32 +0300

    css animation and icons

--- script.js
+++ script.js
@@ -7,10 +7,21 @@ favorites.addEventListener("click", function() {
     location.href = "favorites.html"
 })
 
-box.forEach((paint, index) => {
-    paint.addEventListener("mouseover", function() {
-        const wrapper = document.createElement("div");
-        paint.appendChild(wrapper);
-        console.log(paint)
-    })
-})
+// box.forEach((paint, index) => {
+//     paint.addEventListener("mouseover", function() {
+//         const wrapper = document.createElement("div");
+//         wrapper.classList.add("wrapper");
+//         const textArea = document.createElement("div");
+//         textArea.classList.add("textarea");
+//         textArea.innerText = "Birth of Venus"
+//         paint.appendChild(textArea);
+//         paint.appendChild(wrapper);
+//         // console.log(paint)
+//     })
+//     paint.addEventListener("mouseout", function() {
+//         const wrapper = document.querySelector(".wrapper");
+//         const textArea = document.querySelector(".textarea");
+//         wrapper.classList.remove("wrapper");
+//         textArea.classList.remove("textarea");
+//     })
+// })
