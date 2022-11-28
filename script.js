d86d5662a0634065af729a8ad09e58ac2e447fd9
semakeremetci <keremetcisema@gmail.com>
2022-11-28 16:51:51 +0300

    solved array problem

--- script.js
+++ script.js
@@ -1,16 +1,10 @@
-const logo = document.getElementById("logo");
 const favorites = document.getElementById("favorites");
-const paintings = document.querySelector(".painting");
-const box = document.querySelectorAll(".box");
 const hearts = document.querySelectorAll(".heart");
-const info = document.querySelectorAll("h3");
 const svg = document.querySelectorAll(".svg");
-const data = localStorage.getItem("painting");
 const svgData = localStorage.getItem("svgs");
 // console.log(data)
-const paintingInfo = data ? data.split(",") : [];
-const svgInfo = svgData ? svgData.split(",") : [];
-console.log(svgInfo);
+let svgInfo = svgData ? svgData.split(",") : [];
+// console.log(svgInfo);
 
 
 favorites.addEventListener("click", function() {
@@ -23,19 +17,13 @@ hearts.forEach((item, index) => {
         if(item.classList.contains("heart")) {
             item.classList.replace("heart", "heart_filled")
             // console.log(item.classList)
-            paintingInfo.push(info[index].outerHTML);
-            // console.log(paintingInfo);
-            localStorage.setItem("painting", paintingInfo);
             svgInfo.push(index);
-            // svgs.push(svg[index]);
-            // console.log(svgs);
             localStorage.setItem("svgs", svgInfo);
         } else if(item.classList.contains("heart_filled")) {
             item.classList.replace("heart_filled", "heart")
-            let spliced = paintingInfo.splice(index,1);
-            let splicedSvg = svgInfo.splice(index,1);
-            // console.log(spliced);
-            localStorage.setItem("painting", paintingInfo);
+            let newIndex = svgInfo.indexOf(index);
+            // console.log(newIndex)
+            let splicedSvg = svgInfo.splice(newIndex, 1);
             localStorage.setItem("svgs", svgInfo);
             // console.log(paintingInfo);
         }
@@ -48,7 +36,7 @@ if (svgInfo) {
     // console.log(svg[svgInfo[3]])
     for(let i in svgInfo){
         svg[svgInfo[i]].classList.replace("heart", "heart_filled")
-        console.log(svg[svgInfo[i]])
+        // console.log(svg[svgInfo[i]])
     }
 
 }
