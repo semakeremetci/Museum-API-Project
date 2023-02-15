c6e012cd21032299226f6ef4ade49d7f2eb9419d
semakeremetci <keremetcisema@gmail.com>
2023-02-15 23:35:31 +0300

    remastered

--- favorites.js
+++ favorites.js
@@ -37,3 +37,4 @@ heart.forEach((item, index) => {
         }    
     })
 })
+
--- script.js
+++ script.js
@@ -3,7 +3,8 @@ const hearts = document.querySelectorAll(".heart");
 const svg = document.querySelectorAll(".svg");
 const svgData = localStorage.getItem("svgs");
 let svgInfo = svgData ? svgData.split(",") : [];
-// console.log(svgInfo);
+const inputField = document.querySelector("input");
+const frame = document.querySelector(".frame");
 
 
 favorites.addEventListener("click", function() {
@@ -28,13 +29,37 @@ hearts.forEach((item, index) => {
     })
 })
 
-
-
 if (svgInfo) {
     // console.log(svg[svgInfo[3]])
     for(let i in svgInfo){
         svg[svgInfo[i]].classList.replace("heart", "heart_filled")
         // console.log(svg[svgInfo[i]])
     }
-
 }
+
+
+inputField.addEventListener("keydown",function (e) {
+    if(e.key === "Enter") {
+        if (frame.lastElementChild.classList.contains("input_search")) {
+            frame.removeChild(frame.lastChild)
+            console.log("kaldırıldı")
+        }
+        frame.insertAdjacentHTML("beforeend", "<div class='input_search'></div>")
+
+        const search = inputField.value;
+        fetch("https://openaccess-api.clevelandart.org/api/artworks/?q="+search+ "&has_image=1",{method: "GET"}).then(
+            response => response.json()
+        ).then(
+            (data) => {
+                data.data.forEach(item => {
+                    console.log(item.creators[0].description)
+                    const input_search = document.querySelector(".input_search");
+                    const search_card = document.createElement("div"); search_card.classList.add("search_card"); input_search.appendChild(search_card);
+                    const card_image = document.createElement("div"); card_image.classList.add("card_image"); search_card.appendChild(card_image);
+                    search_card.insertAdjacentHTML("beforeend","<p class='card_name'>" + "<strong>" + item.title + "</strong></p>" + "<p class='card_description'>" + item.creators[0].description + "</p>")
+                    card_image.style.backgroundImage = "url("+ item.images.web.url + ")"
+                })
+            }
+        ).catch(err => console.log(err));
+            }
+})
